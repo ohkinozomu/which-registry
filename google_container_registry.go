8ef576b93ac9fb3e84d1a3fbb216878e3a8656ba
ohkinozomu <nozomunoise@gmail.com>
2022-02-17 06:34:26 +0900

    Add GCP region validation

--- google_container_registry.go
+++ google_container_registry.go
@@ -1,13 +1,24 @@
 package which_registry
 
-import "regexp"
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
 
 // https://cloud.google.com/container-registry/docs/overview#registries
 func isGoogleContainerRegistry(d string) (bool, error) {
-	// TODO: location check
 	match, err := regexp.MatchString(".*gcr.io", d)
 	if err != nil {
 		return false, err
+	}
+
+	if match {
+		location := strings.Replace(d, "gcr.io", "", 1)
+		if location == "" || location == "us." || location == "eu." || location == "asia." {
+			return true, nil
+		}
+		return false, errors.New("Invalid location: " + location)
 	}
 	return match, nil
 }
